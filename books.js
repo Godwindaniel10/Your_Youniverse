9525310e7745d538d03041fbf7f1233c4e630757
godwin olele <godwinolele10@gmail.com>
2021-09-26 14:17:22 +0100

    books section

--- books.js
+++ books.js
@@ -11,10 +11,10 @@ book1.addEventListener("mouseout" , function(){
 /* book2 */
 const book2 = document.getElementById("book2");
 book2.addEventListener("mouseover" , function(){
-  document.getElementById("book3p").style.display = "block";
+  document.getElementById("book2p").style.display = "block";
 });
 book2.addEventListener("mouseout" , function(){
-  document.getElementById("book1p").style.display = "none";
+  document.getElementById("book2p").style.display = "none";
 });
 
 
@@ -26,4 +26,106 @@ book3.addEventListener("mouseover" , function(){
 });
 book3.addEventListener("mouseout" , function(){
   document.getElementById("book3p").style.display = "none";
+});
+
+
+/* book4 */
+const book4 = document.getElementById("book4");
+book4.addEventListener("mouseover" , function(){
+  document.getElementById("book4p").style.display = "block";
+});
+book4.addEventListener("mouseout" , function(){
+  document.getElementById("book4p").style.display = "none";
+});
+
+
+/* book5 */
+const book5 = document.getElementById("book5");
+book5.addEventListener("mouseover" , function(){
+  document.getElementById("book5p").style.display = "block";
+});
+book5.addEventListener("mouseout" , function(){
+  document.getElementById("book5p").style.display = "none";
+});
+
+
+/* book6 */
+const book6 = document.getElementById("book6");
+book6.addEventListener("mouseover" , function(){
+  document.getElementById("book6p").style.display = "block";
+});
+book6.addEventListener("mouseout" , function(){
+  document.getElementById("book6p").style.display = "none";
+});
+
+
+
+/* book7 */
+const book7 = document.getElementById("book7");
+book7.addEventListener("mouseover" , function(){
+  document.getElementById("book7p").style.display = "block";
+});
+book7.addEventListener("mouseout" , function(){
+  document.getElementById("book7p").style.display = "none";
+});
+
+
+/* book8 */
+const book8 = document.getElementById("book8");
+book8.addEventListener("mouseover" , function(){
+  document.getElementById("book8p").style.display = "block";
+});
+book8.addEventListener("mouseout" , function(){
+  document.getElementById("book8p").style.display = "none";
+});
+
+
+/* book9 */
+const book9 = document.getElementById("book9");
+book9.addEventListener("mouseover" , function(){
+  document.getElementById("book9p").style.display = "block";
+});
+book9.addEventListener("mouseout" , function(){
+  document.getElementById("book9p").style.display = "none";
+});
+
+
+/* book10 */
+const book10 = document.getElementById("book10");
+book10.addEventListener("mouseover" , function(){
+  document.getElementById("book10p").style.display = "block";
+});
+book10.addEventListener("mouseout" , function(){
+  document.getElementById("book10p").style.display = "none";
+});
+
+
+
+/* book11 */
+const book11 = document.getElementById("book11");
+book11.addEventListener("mouseover" , function(){
+  document.getElementById("book11p").style.display = "block";
+});
+book11.addEventListener("mouseout" , function(){
+  document.getElementById("book11p").style.display = "none";
+});
+
+
+/* book12 */
+const book12 = document.getElementById("book12");
+book12.addEventListener("mouseover" , function(){
+  document.getElementById("book12p").style.display = "block";
+});
+book12.addEventListener("mouseout" , function(){
+  document.getElementById("book12p").style.display = "none";
+});
+
+
+/* book13 */
+const book13 = document.getElementById("book13");
+book13.addEventListener("mouseover" , function(){
+  document.getElementById("book13p").style.display = "block";
+});
+book13.addEventListener("mouseout" , function(){
+  document.getElementById("book13p").style.display = "none";
 });
